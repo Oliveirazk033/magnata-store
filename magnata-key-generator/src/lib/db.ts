1b9c4e4b3243be09a1d739790a61ce521ad3183d
Z User <z@container>
2026-07-10 00:28:40 +0000

    24b27f6c-9998-4528-9386-d698caa71f23

--- magnata-key-generator/src/lib/db.ts
+++ magnata-key-generator/src/lib/db.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
+
+function createDb() {
+  const dbUrl = process.env.DATABASE_URL
+  if (!dbUrl) throw new Error('DATABASE_URL is not set')
+
+  const libsql = createClient({ url: dbUrl })
+  const adapter = new PrismaLibSQL(libsql)
+  return new PrismaClient({ adapter })
+}
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient()
+export const db = globalForPrisma.prisma ?? createDb()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
